6acc07615b0f649d6913bed692c0ccac0d72d1b9
Joe Keenan <>
2018-07-23 21:05:42 -0400

    daily

---
+++ 2.c
@@ -0,0 +1,43 @@
+
+/*
+def fib(fibToFind):
+    if fibToFind == 1 :
+        return ( 1 )
+    elif fibToFind == 0 :
+        return ( 0 )
+    else:
+        return fib( fibToFind - 1 ) + fib( fibToFind - 2 )
+
+def fibForReal():
+    fibToFind = int(raw_input( "Enter A Fibonacci Number That You Would Like To See " )) - 1
+    mylist = fib(fibToFind)
+    return "The " + str( fibToFind + 1 ) + "th Fibonacci Number Is " + str( mylist ) + "!!!"
+*/
+
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+
+// int someFib(int num) {
+ for (int i=1; i<)
+//
+  // return num;
+//}
+
+int main (void){
+int c=get_int("Enter a Number:");
+int a=c%2;
+int z=someFib(c);
+printf("c= %i  a=%i\n",c,a);
+
+
+
+
+}
+
+
+
+
